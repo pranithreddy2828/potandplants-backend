79c3e350c5999c47e269e7c6d4ff5d1570cc53b2
pranithreddy2828 <pranith.quantumquill@gmail.com>
2026-03-27 00:56:21 +0530

    add root route for health check

--- index.js
+++ index.js
@@ -12,6 +12,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/', (req, res) => res.send('Pot & Plants API is running... Use /api prefix for data.'));
+
+
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log('Connected to MongoDB'))
   .catch(err => console.error('Error connecting to MongoDB:', err));
